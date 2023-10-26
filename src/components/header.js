4dd6c44d5d029a7ba07e7a0aad68451cc2a627ee
Adarsh <adarsh.pal@myrealdata.in>
2023-10-26 16:49:10 +0530

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header(){
+
+    return (
+        <>
+
+            <div className="App">
+                <h1>Just a normal heading!</h1>
+            </div>
+            
+        </>
+    );
+}
+
+export default Header;
---
+++ src/components/navbar.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class Navbar extends Component {
+
+    render(){
+        return (
+            <ul className="list">
+                <li>Home</li> &nbsp;
+                <li>About Us</li>&nbsp;
+                <li>Contact</li>
+            </ul>
+        )
+    }
+
+
+}
+
+export default Navbar;
